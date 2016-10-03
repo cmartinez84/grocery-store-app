eb72355843c08178363c313bcfbf5103fd63751b
Angie Smith and chris martinez <angie@>
2016-10-03 09:26:33 -0700

    begin grocery store app project with empty folders

---
+++ src/Product.php
@@ -0,0 +1,12 @@
+<?php
+    Class Product
+    {
+        private $name;
+        private $price;
+        private $purchase_quantity;
+        private $inventory;
+        private $category;
+        private $photo;
+        private $id;
+    }
+ ?>
---
+++ web/index.php
@@ -0,0 +1,18 @@
+<?php
+    $website = require_once __DIR__.'/../app/app.php';
+    $website->run();
+ ?>
+ <!DOCTYPE html>
+ <html>
+   <head>
+      <link href="https://fonts.googleapis.com/css?family=Anton|Open+Sans|Pacifico|Permanent+Marker" rel="stylesheet">
+     <link rel="stylesheet" href="/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+      <link rel="stylesheet" href="/css/mystyles.css" media="screen" title="no title" charset="utf-8">
+
+     <script src="/js/jquery-3.1.0.min.js"></script>
+     <script src="/js/myscripts.js"></script>
+   </head>
+   <body>
+
+   </body>
+ </html>
