1cd7c45cf05d23382cbc8b7d87fe5b44c55bc8ac
Angie Smith and chris martinez <chris@>
2016-10-03 11:25:28 -0700

    add calculations for the price of each product.

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -291,5 +291,61 @@
             $this->assertEquals([], $result);
         }
 
+        function testFind()
+        {
+            $test_name = "apple";
+            $test_price = 1.00;
+            $test_purchase_quantity = 2;
+            $test_inventory = 5;
+            $test_photo = "";
+            $test_id = null;
+            $test_product = new Product($test_name, $test_price, $test_purchase_quantity, $test_inventory, $test_photo, $test_id);
+            $test_product->save();
+
+            $test_name2 = "lettuce";
+            $test_price2 = .75;
+            $test_purchase_quantity2 = 1;
+            $test_inventory2 = 5;
+            $test_photo2 = "";
+            $test_id2 = null;
+            $test_product2 = new Product($test_name2, $test_price2, $test_purchase_quantity2, $test_inventory2, $test_photo2, $test_id2);
+            $test_product2->save();
+
+            $result = Product::find($test_product2->getId());
+
+            $this->assertEquals($test_product2, $result);
+        }
+
+        function testInStock()
+        {
+            $test_name = "apple";
+            $test_price = 1.00;
+            $test_purchase_quantity = 3;
+            $test_inventory = 5;
+            $test_photo = "";
+            $test_id = null;
+            $test_product = new Product($test_name, $test_price, $test_purchase_quantity, $test_inventory, $test_photo, $test_id);
+            $test_product->save();
+
+            $result = $test_product->inStock($test_purchase_quantity);
+
+            $this->assertEquals(true, $result);
+        }
+
+        function testCalculateProductPrice()
+        {
+            $test_name = "apple";
+            $test_price = 1.00;
+            $test_purchase_quantity = 3;
+            $test_inventory = 5;
+            $test_photo = "";
+            $test_id = null;
+            $test_product = new Product($test_name, $test_price, $test_purchase_quantity, $test_inventory, $test_photo, $test_id);
+            $test_product->save();
+
+            $result = $test_product->calculateProductPrice($test_purchase_quantity);
+
+            $this->assertEquals(3.00, $result);
+        }
     }
 ?>
