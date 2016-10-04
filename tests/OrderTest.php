43f0a624d278da7551b2e1efec3c08fdefbf2d72
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-10-03 21:55:43 -0700

    tests pass to inetgrate order and product price functions

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -6,6 +6,8 @@
     */
 
     require_once "src/Order.php";
+    require_once "src/Product.php";
+
 
     $server = 'mysql:host=localhost:8889;dbname=shoppr';
     $username = 'root';
@@ -16,6 +18,7 @@
         protected function teardown()
         {
             Order::deleteAll();
+            Product::deleteAll();
         }
 
         function test_getId()
@@ -88,7 +91,6 @@
             $result = Order::getAll();
 
             $this->assertEquals([$test_order, $test_order2], $result);
-
         }
 
         function test_find()
@@ -156,9 +158,57 @@
 
             $this->assertEquals([], $result);
 
-
-
         }
+
+        function test_addProductToCart()
+        {
+            $id = null;
+            $user_id = 1;
+            $order_date = "2016-10-01";
+            $delivery_date_time = "2016-10-02";
+            $test_order = new Order($id, $user_id, $order_date, $delivery_date_time);
+            $test_order->save();
+
+            $test_name = "apple";
+            $test_price = 1.00;
+            $test_purchase_quantity = 3;
+            $test_inventory = 5;
+            $test_photo = "";
+            $test_id = null;
+            $test_product = new Product($test_name, $test_price, $test_purchase_quantity, $test_inventory, $test_photo, $test_id);
+
+            //Act
+            $test_order->addProductToCart($test_product);
+            $result = $test_order->getCart();
+            $this->assertEquals([$test_product], $result);
+        }
+        function test_getCartTotal()
+        {
+            $id = null;
+            $user_id = 1;
+            $order_date = "2016-10-01";
+            $delivery_date_time = "2016-10-02";
+            $test_order = new Order($id, $user_id, $order_date, $delivery_date_time);
+            $test_order->save();
+
+
+            $test_purchase_quantity = 3;
+            $test_price = 1.00;
+            $test_product = new Product("apple", $test_price, $test_purchase_quantity, 9, "", null);
+
+            $test_purchase_quantity2 = 2;
+            $test_price2 = 2.00;
+            $test_product2 = new Product("apple", $test_price2, $test_purchase_quantity2, 9, "", null);
+
+            //
+            // //Act
+            $test_order->addProductToCart($test_product);
+            $test_order->addProductToCart($test_product2);
+            $result =$test_order->getCartTotal();
+            echo $result;
+            $this->assertEquals(7, $result);
+            }
+
 
     }
 ?>
