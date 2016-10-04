23eeea705e5677419d0348f4c97fbcde297d2734
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-10-04 16:16:31 -0700

    about to work on member log in branch

---
+++ app/appLogin.php
@@ -0,0 +1,29 @@
+<?php
+
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Customer.php";
+    date_default_timezone_set('America/Los_Angeles');
+
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    use Symfony\Component\HttpFoundation\Request;  Request::enableHttpMethodParameterOverride();
+
+    $app = new Silex\Application();
+
+    $app['debug'] = true;
+
+    $server = 'mysql:host=localhost:8889;dbname=shoppr';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('newMember.html.twig');
+    });
+
+    return $app;
+?>
