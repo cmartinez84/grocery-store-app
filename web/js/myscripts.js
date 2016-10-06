5e6183ffed43a94418761d26e0129fb23de38fdd
Angie Smith and April Peng <angie+april@>
2016-10-05 19:34:51 -0700

    add cart hisotry to user, all display on accordian menu

--- web/js/myscripts.js
+++ web/js/myscripts.js
@@ -1,37 +1,11 @@
 $(function(){
 
-    $(".infoTop").click(function(){
-        $("div").removeClass("outline");
-        $("span").removeClass("rotateIcon");
-        $(".show").removeClass("show");
-        $(this).parent().toggleClass("outline");
-        // $(this).children(".glyphicon").toggleClass("rotateIcon");
-    });
+
 
     $(".accordian").click(function(){
         $(this).next().toggleClass("show");
-        $(this).children(".glyphicon").toggleClass("rotateIcon");
+        // alert("ouch");
+        // $(this).children(".glyphicon").toggleClass("rotateIcon");
 
     });
 });
-
-<a href="javascript:deleteProduct({$product->id})">Delete Product</a>
-
-function deleteProduct(id) {
-    $.ajax({
-        type:'DELETE',
-        url:'/cart/delete/'+id,
-        success:function(html) {
-            $('#cart').html(html);
-        }
-    });
-}
-/*function updateProduct() {
-    $.ajax({
-        type:'PATCH',
-        url:'/cart/edit',
-        success:function(html) {
-            $('#cart').html(html);
-        }
-    });
-}*/
